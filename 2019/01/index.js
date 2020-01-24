452d83a30ee56c08abd4210e9103223f391590f9
LepkoQQ <lepko.san@gmail.com>
2020-01-24 14:31:21 +0100

    2019 day 1

--- 2019/01/index.js
+++ 2019/01/index.js
@@ -2,4 +2,29 @@ const { readToString } = require('../utils');
 
 const input = readToString(`${__dirname}/input`);
 
-console.log(input);
+const masses = input
+  .trim()
+  .split('\n')
+  .map(Number);
+
+const fuels = masses.map((m) => Math.floor(m / 3) - 2);
+const totalFuel = fuels.reduce((sum, cur) => sum + cur, 0);
+
+console.log('part 1 =', totalFuel);
+
+const fuels2 = masses.map((m) => {
+  let sum = 0;
+  let f = m;
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    f = Math.floor(f / 3) - 2;
+    if (f <= 0) {
+      break;
+    }
+    sum += f;
+  }
+  return sum;
+});
+const totalFuel2 = fuels2.reduce((sum, cur) => sum + cur, 0);
+
+console.log('part 2 =', totalFuel2);
