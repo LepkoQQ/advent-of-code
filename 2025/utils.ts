710a1e0746833cb0e7e7c4173929205b74da2f47
LepkoQQ <miha@lepko.net>
2025-12-01 17:41:30 +0100

    add template for 2025

---
+++ 2025/utils.ts
@@ -0,0 +1,20 @@
+import fs from "node:fs"
+import path from "node:path"
+
+export function loadInput(scriptUrl: string): string {
+  return fs
+    .readFileSync(
+      path.join(path.dirname(new URL(scriptUrl).pathname), "input.txt"),
+      "utf-8",
+    )
+    .trim()
+}
+
+export function dirs4(): [number, number][] {
+  return [
+    [0, -1],
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+  ]
+}
